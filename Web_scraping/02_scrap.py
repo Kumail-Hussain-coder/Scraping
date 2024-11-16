dd67c878426dfd2960126e09b31d6f945839e699
Kumail-Hussain-coder <kh478341@gmail.com>
2024-11-16 10:37:49 -0800

    Extract Paragraph

---
+++ Web_scraping/02_scrap.py
@@ -0,0 +1,14 @@
+# Only we take Paragraph from Website
+# To extract the data we use bs4(BeautifulSoup4)
+import requests
+import bs4
+import sys
+
+sys.stdout.reconfigure('utf-8')
+url = "https://open.spotify.com/"
+data = requests.get(url)
+soup = bs4.BeautifulSoup(data.text, 'html.parser')
+
+# To take the paragraph we us loop
+for para in soup.find_all('p'):
+    print(para)
